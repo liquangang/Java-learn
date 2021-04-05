da19ef22d0944f2888424cda2c918e95b0c3034e
liquangang <1347336730@qq.com>
2021-04-05 15:49:15 +0800

    proj init

---
+++ learn-demo/src/test/java/com/liquangang/learndemo/LearnDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liquangang.learndemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
